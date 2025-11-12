526dd6d95695b02d3adeadf63457fc9214e5ae42
qwewwq <ironou7@gmail.com>
2025-11-12 14:40:53 +0300

    add db connection

---
+++ app/db/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from app.core.config import settings
+from app.models.base import Base
+
+engine = create_async_engine(settings.database_url, connect_args={"ssl": None})
+SessionLocal = async_sessionmaker(engine)
+
+
+async def get_db():
+	async with engine.begin() as conn:
+		await conn.run_sync(Base.metadata.create_all)
+	
+	db = SessionLocal()
+	try:
+		yield db
+	finally:
+		await db.close()
