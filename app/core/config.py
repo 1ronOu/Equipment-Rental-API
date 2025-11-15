411e390e252bec723219a050c2e66c713c08cfa3
qwewwq <ironou7@gmail.com>
2025-11-15 13:14:32 +0300

    change config file for redis

--- app/core/config.py
+++ app/core/config.py
@@ -10,8 +10,12 @@ class Settings(BaseSettings):
 	app_port: int = Field(default=8000)
 
 	database_url: str 
-	redis_url: str 
-	
+	redis_password: str
+	redis_user: str
+	redis_user_password: str
+	redis_host: str
+	redis_port: int
+
 	jwt_secret_key: str 
 	jwt_algorithm: str = Field(default="HS256")
 
