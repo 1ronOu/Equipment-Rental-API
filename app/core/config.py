5992d10b55c8ea212d0f52eddba8d794ae7ed1ba
qwewwq <ironou7@gmail.com>
2025-11-18 16:20:43 +0300

    add jwt keys paths to config

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+class AuthJWT(BaseSettings):
+	private_key_path: Path = BASE_DIR / 'certs' / 'jwt_private.pem'
+	public_key_path: Path = BASE_DIR / 'certs' / 'jwt_public.pem'
 
 
 class Settings(BaseSettings):
@@ -9,15 +18,17 @@ class Settings(BaseSettings):
 	app_host: str = Field(default="127.0.0.1")
 	app_port: int = Field(default=8000)
 
-	database_url: str 
+	database_url: str
 	redis_password: str
 	redis_user: str
 	redis_user_password: str
 	redis_host: str
 	redis_port: int
 
-	jwt_secret_key: str 
+	jwt_secret_key: str
 	jwt_algorithm: str = Field(default="HS256")
+
+	auth_jwt: AuthJWT = AuthJWT()
 
 
 
