34b3133c98629c4598700ced8dbd95b3f5bd878d
qwewwq <ironou7@gmail.com>
2025-11-25 14:35:43 +0300

    delete unused imports, make UserSchema and UserUpdate

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -1,5 +1,5 @@
+
 from pydantic import BaseModel, EmailStr, ConfigDict, Field
-from fastapi import Query
 
 
 class UserCreate(BaseModel):
@@ -13,3 +13,18 @@ class UserRead(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     name: str = Field(max_length=30)
     email: EmailStr = Field(max_length=30)
+    role: str = Field(max_length=30)
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: str = Field(max_length=30)
+    email: EmailStr = Field(max_length=30)
+    password: bytes
+    role: str = Field(max_length=30)
+
+
+class UserUpdate(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: str | None = Field(default=None, max_length=30)
+    email: EmailStr | None = Field(default=None, max_length=30)
