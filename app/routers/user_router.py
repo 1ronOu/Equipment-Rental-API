3122a7528033cf7afb59166eb052f29e4ad9e1d1
qwewwq <ironou7@gmail.com>
2025-11-25 14:46:02 +0300

    make some secure endpoints

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,12 +1,18 @@
 from typing import List
 
+from attr.filters import exclude
 from fastapi import APIRouter, Depends, Query
+from fastapi.security import HTTPBearer
 from pydantic import EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.schemas.user_schemas import UserCreate, UserRead
+from app.admin.admin_tools import require_admin
+from app.schemas.user_schemas import UserCreate, UserRead, UserSchema, UserUpdate
 from app.services import user_services
 from app.db.db import get_db
+from app.auth import auth_utils
+
+http_bearer = HTTPBearer(auto_error=False)
 
 router = APIRouter(
     prefix="/users",
@@ -14,9 +20,8 @@ router = APIRouter(
 )
 
 
-@router.get("/{user_id}", response_model=UserRead)
-async def read_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    user = await user_services.user_read(user_id=user_id, db=db)
+@router.get("/me", response_model=UserRead)
+async def read_user(user: UserSchema = Depends(auth_utils.get_current_user)):
     return user
 
 
@@ -32,18 +37,22 @@ async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     return new_user
 
 
-@router.patch('/{user_id}', response_model=UserRead)
+@router.patch('/me', response_model=UserRead)
 async def update_user(
-        user_id: int,
-        new_name: str | None = Query(default=None, max_length=30),
-        new_email: EmailStr | None = Query(default=None, max_length=30),
+        fields_to_update: UserUpdate,
+        user: UserSchema = Depends(auth_utils.get_current_user),
         db: AsyncSession = Depends(get_db)
 ):
-    updated_user = await user_services.user_update(user_id=user_id, new_name=new_name, new_email=new_email, db=db)
+    updated_user = await user_services.user_update(
+        user_id=user.id,
+        new_name=fields_to_update.name,
+        new_email=fields_to_update.email,
+        db=db
+    )
     return updated_user
 
 
 @router.delete('/{user_id}')
-async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
+async def delete_user(user_id: int, user: UserSchema = Depends(require_admin), db: AsyncSession = Depends(get_db)):
     deleted_user = await user_services.user_delete(user_id=user_id, db=db)
     return deleted_user
