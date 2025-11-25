1e1dc53559747665a28db005a82a639582e2e0fd
qwewwq <ironou7@gmail.com>
2025-11-25 14:34:07 +0300

    change email field to unique, add role field

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -8,5 +8,6 @@ class User(Base):
 	__tablename__ = 'users'
 
 	name: Mapped[str] = mapped_column(String(30))
-	email: Mapped[str] = mapped_column(String(30))
-	password: Mapped[str] = mapped_column(String(30))
+	email: Mapped[str] = mapped_column(String(30), unique=True)
+	password: Mapped[bytes]
+	role: Mapped[str] = mapped_column(String(30), default='user')
